22728445e68c86f764d7aeb8b96bd17089bd11b8
Muhammad Younis <39456563+muhammadyounispk@users.noreply.github.com>
2022-10-28 19:41:27 +0500

    Update codeWriter.php

--- codeWriter.php
+++ codeWriter.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'database.php';
+include_once '../common/database.php';
  class codeWriter{
 	public  $table_name="null";
 	public $where=null;
@@ -14,18 +14,21 @@ include_once 'database.php';
 	public $data=null;
 	public $file_name="null";
 	public  $code=null;
-	function __construct($table_name=null,$where=null) {
+	function __construct($table_name=null) {
 		if($table_name){
 			$this->table_name = $table_name;
-			$this->where = $where;
 			$this->query="show  COLUMNS from  $table_name ";
-			$this->schema = db::getRecords($this->query);
-					}
+			$this->schema = db::getRecords($this->query);}
+	
 	  }
-	  public function set_output($file_name){
+	  public function Where($where=null){
+		$this->where = $where;
+		
+	  }
+	  public function set_output($file_name=null){
         $this->file_name=$file_name;
 	  }
-	public  function cols($cols_name) {
+	public  function cols($cols_name=null) {
 	$this->arr_selection=explode(",",chop($cols_name,","));
 	foreach ($this->schema as $key => $value) {
 		$col = $value['Field'];
@@ -37,7 +40,8 @@ include_once 'database.php';
 	}
 
 	  }
-	  public function write_function($function_name = "display",$params=null,$return_type=null){
+public function write_function($function_name = "display",$params=null,$return_type=null){
+
 	 $this->return_type=$return_type;
      $params?  $this->params='$'.str_replace(',',',$',$params): '';
      $this->function_name=$function_name;
@@ -50,7 +54,6 @@ foreach ($this->schema as $key => $value) {
 	$this->data .= '\'' . $col_name . '\'=> $record[\'' . $col_name . '\'],
 ';
 }
-
 
 		$code .= '
 //preparing Database Query
@@ -71,11 +74,69 @@ return json_encode($obj);
 
 
 		break;
+		case 'fetch';
+
+		foreach ($this->schema as $key => $value) {
+			$col_name = $value['Field'];
+			$this->data .= '$' . $col_name . '= $record[\'' . $col_name . '\'];
+';
+		}
+		
+		
+				$code .= '
+//preparing Database Query
+$query="SELECT ' . $this->cols . ' from ' . $this->table_name . ' '.$this->where.' ";
+//getting records in Assosiative Array
+$records=db::getRecords($query);
+//extracting Information 
+foreach($records as $record){ 
+//preparing custom Variables Assosiative Array
+'
+. $this->data . '
+
+		}
+		
+				
+				';
+
+		break;
+		case 'insert_POST';
+		//post vars
+		$postVar="";
+		foreach ($this->schema as $key => $value) {
+			$col_name = $value['Field'];
+			$postVar.= '$'.$col_name.'=$_POST[\'' . $col_name . '\'];
+';
+}
+
+//cols values
+$obj_arr="";
+foreach ($this->schema as $key => $value) {
+	$col_name = $value['Field'];
+$obj_arr.='$obj[\''.$col_name.'\']=$_POST[\''.$col_name.'\'];
+';
+}
+$code.=$obj_arr;
+$code.='$query=db::prepInsertQuery($obj,\''.$this->table_name .'\');
+';
+$code.='$result=db::insertRecord($obj,\''.$this->table_name .'\');
+';
+
+$this->data=$code;
+
+
+
+
+
+
+		break;
 		default:
 		$code.=$this->data;
 
        }
-$code.= '}';
+$code.= '
+
+}';
 
 codeWriter::write($code);
 
@@ -93,7 +154,13 @@ $this->write($this->data);
 		}else{
 			codeWriter::show("You have no table Selected to write code ");
 		}
-		 }
+		 }	  
+
+	  /**
+	   * Method obj_col_vars
+	   *
+	   * @return void
+	   */
 	  public function obj_col_vars()
 	  {
 		if(isset($this->schema )){
@@ -103,13 +170,24 @@ $this->data.='$obj[]=$record["'.$col_name.'"];
 ';
 }
 		}else{
-			codeWriter::show("You have no table Selected to write code ");
+			codeWriter::show("You have no table Selected to write code or Column Name(s) not found in  " .$this->table_name );
+			die();
+		}
+		 }
+	  public function col_vars()
+	  {
+		if(isset($this->schema )){
+foreach ($this->schema as $key => $value) {
+$col_name=$value['Field'];
+$this->data.='$'.$col_name.'=$record["'.$col_name.'"]; 
+';
+}
+		}else{
+			codeWriter::show("You have no table Selected to write code or Column Name(s) not found in  " .$this->table_name);
 			die();
 		}
 		 }
 		 
-
-
 
 		 function http_vars($vars=null,$type=null){
 			if($type=='1'){
@@ -138,7 +216,7 @@ if($type){
 		 }
 
 	  
-  public function write($data=null){
+public function write($data=null){
 	if(isset($data)){
 		file_put_contents($this->file_name, $data, FILE_APPEND);
 		codeWriter::show("$data<br>records  been added into ".$this->file_name);
@@ -148,20 +226,13 @@ if($type){
 
 
 	}
-  }	  
-  public function show($data=null){
+}	  
+public function show($data=null){
 	if($data){
 	echo "<pre>";
 	print_r($data);
 	echo "</pre>";
 		
 	}
-  }	  
-	
+  }	  	
 }
-
-
-
-
-
-
