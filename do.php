0f656ce5da5ce07b6edeabfb6763f28accf04d9a
Muhammad Younis <39456563+muhammadyounispk@users.noreply.github.com>
2022-10-28 19:42:19 +0500

    Update do.php

--- do.php
+++ do.php
@@ -1,24 +1,8 @@
 <?php
 include_once 'codeWriter.php';
-include_once '../common/database.php';
+include_once 'database.php';
 $codeWriter=new codeWriter("stories");
 $codeWriter->set_output("do.php");
-$codeWriter->obj_col_vars();
-$codeWriter->write();
+$codeWriter->write_function("insert_Data",null,'insert_POST');
 
-$obj[]=$record["id"]; 
-$obj[]=$record["video_id"]; 
-$obj[]=$record["title"]; 
-$obj[]=$record["description"]; 
-$obj[]=$record["video_url"]; 
-$obj[]=$record["image_name"]; 
-$obj[]=$record["length"]; 
-$obj[]=$record["owner_id"]; 
-$obj[]=$record["created_date"]; 
-$obj[]=$record["modified_date"]; 
-$obj[]=$record["genre"]; 
-$obj[]=$record["topic"]; 
-$obj[]=$record["upvotes"]; 
-$obj[]=$record["downvotes"]; 
-$obj[]=$record["is_paid"]; 
-$obj[]=$record["display_order"]; 
+
